2bc36fcd598e5850f3f074fa142aefde218bae0d
G00403364@atu.ie <G00403364@atu.ie>
2025-10-20 10:19:04 +0100

    Simple try / catch example for age.

--- src/main/java/ie/atu/week5/TryMain.java
+++ src/main/java/ie/atu/week5/TryMain.java
@@ -6,6 +6,18 @@ import java.util.List;
 public class TryMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int age = 0;
+        while(true) {
+            System.out.println("Enter your age: ");
+            String text = sc.nextLine().trim();
+            try{
+                age = Integer.parseInt(text);
+                System.out.println("Thanks! You entered the age: " + age);
+                break;
+            }catch (NumberFormatException e) {
+                System.out.println("Invalid input: " + e.getMessage());
+            }
+        }
 
     }
 }
