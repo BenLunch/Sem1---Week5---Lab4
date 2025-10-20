895dd49c94cc77f81dbd19b4f50ff0dd3e242e1c
G00403364@atu.ie <G00403364@atu.ie>
2025-10-20 09:58:26 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/week5/TryMain.java
@@ -0,0 +1,11 @@
+package ie.atu.week5;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TryMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
